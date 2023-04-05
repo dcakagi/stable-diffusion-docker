80cc46c46f3f00a76ae2288fe8ca6892a90bd824
David Akagi <dcakagi@gmail.com>
2023-04-04 23:08:55 -0600

    New prompt

--- run_stablediff.sh
+++ run_stablediff.sh
@@ -2,4 +2,4 @@
 
 source /opt/conda/etc/profile.d/conda.sh
 conda activate ldm
-python3 scripts/txt2img.py --prompt "a photograph of an underwater car being driven by a fish" --plms
+python3 scripts/txt2img.py --prompt "kids swimming in a fish tank" --plms
